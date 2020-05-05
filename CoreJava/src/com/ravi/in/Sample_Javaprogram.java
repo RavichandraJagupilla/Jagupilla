51e9f68c36c897397f19bb824c262c6c345c40a3
Welcome <Welcome@hp>
2020-05-05 11:25:04 +0530

    First Project commit

---
+++ CoreJava/src/com/ravi/in/Sample_Javaprogram.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ravi.in;
+
+/**
+ * @author Welcome
+ *
+ */
+public class Sample_Javaprogram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Java");
+	}
+
+}
